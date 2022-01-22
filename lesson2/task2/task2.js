3fa9cb66dbd6c88174db8a4dad36c69ea0aa0485
Vladislav A <akkv93@gmail.com>
2022-01-22 18:25:06 +0300

    Переделал lesson2_task2

--- lesson2/task2/task2.js
+++ lesson2/task2/task2.js
@@ -1,4 +1,17 @@
-let symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function* generateSequence(start, end) {
+    for (let i = start; i <= end; i++) yield i;
+}
+function* generateAlphaNum() {
+    yield* generateSequence(48, 57); // 0..9
+    yield* generateSequence(65, 90); // A..Z
+    yield* generateSequence(97, 122); // a..z
+}
+let symbols = '';
+
+for (let code of generateAlphaNum()) {
+    symbols += String.fromCharCode(code);
+}
+
 function pswdGenerator (length) {
     let pswd = '';
     for (let i = 0; i < length; i++) {
@@ -7,3 +20,7 @@ function pswdGenerator (length) {
     return pswd;
 }
 console.log(pswdGenerator(20))
+
+// Мы можем сформировать строку символов с помощью генератора, но не совсем понимаю зачем это нужно
+// Или случайный пароль нужно отдать с помощью генератора?
+// Чем это будет лучше?
